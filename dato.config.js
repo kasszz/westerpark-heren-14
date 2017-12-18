15f462b181fbc50389ee456f152c29519425c3a7
Casper Boutens <casper@voorhoede.nl>
2017-12-18 15:28:23 +0100

    chore(dato-cms): get data from data dump

--- dato.config.js
+++ dato.config.js
@@ -1,3 +1,30 @@
 module.exports = (dato, root) => {
+  root.createDataFile('data/players.json', 'json', itemsToJson(dato.players))
+  root.createDataFile('data/matches.json', 'json', itemsToJson(dato.matches))
+}
 
+function itemsToJson (items) {
+  return items.map(itemToJson)
+}
+
+function itemToJson (item) {
+  const itemJson = item.toMap()
+  return removeSeoMetaTags(itemJson)
+}
+
+function removeSeoMetaTags (item) {
+  if (item && item.seoMetaTags) {
+    delete item.seoMetaTags
+  }
+
+  if (typeof item === 'object') {
+    Object.keys(item).forEach(key => {
+      if (Array.isArray(item[key])) {
+        item[key].forEach(removeSeoMetaTags)
+      } else if (item[key] && typeof item[key] === 'object') {
+        removeSeoMetaTags(item[key])
+      }
+    })
+  }
+  return item
 }
