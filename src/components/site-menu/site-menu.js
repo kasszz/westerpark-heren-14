88d544f65b6ba14039fe23ced8dea5d9acdcd518
Casper Boutens <c.boutens@hotmail.com>
2017-12-30 01:09:38 +0100

    feat(site-header): add site menu background, correct spacing stats (#14)
    * feat(site-header): add site menu background, correct spacing stats
    
    * fix(stat-block): edit comment

--- src/components/site-menu/site-menu.js
+++ src/components/site-menu/site-menu.js
@@ -3,18 +3,24 @@ import { hasClass, addClass, removeClass } from '../../lib/class-manipulation'
 
 const selector = '[data-site-menu]'
 const toggleButtonSelector = '[data-site-menu-toggle]'
+const backgroundSelector = '[data-site-menu-background]'
 const isOpenClass = 'site-menu--open'
 const isSupported = ('addEventListener' in window)
 
 function enhance (element) {
   const toggles = listElements(element, toggleButtonSelector)
+  const background = element.querySelector(backgroundSelector)
 
-  toggles.forEach(toggle => {
-    toggle.addEventListener('click', event => {
-      hasClass(element, isOpenClass) ? removeClass(element, isOpenClass) : addClass(element, isOpenClass)
-      event.preventDefault()
-    })
-  })
+  toggles.forEach(toggle => toggle.addEventListener('click', event => {
+    event.preventDefault()
+    toggleClass(element)
+  }))
+
+  background.addEventListener('click', () => toggleClass(element))
+}
+
+function toggleClass (element) {
+  hasClass(element, isOpenClass) ? removeClass(element, isOpenClass) : addClass(element, isOpenClass)
 }
 
 function enhanceWithin (context) {
