b2fd4cc4c097e7ce2a37bec9738728cc7dba298a
Casper Boutens <c.boutens@hotmail.com>
2018-01-10 16:01:42 +0100

    Feat/match list (#16)
    * WIP(match-list): add html
    
    * WIP(match-card): add more html and data to DatoCMS
    
    * WIP(match-card): add static score
    
    * WIP(match-card): add score component
    
    * feat(match-list): add default color to prevent green hover

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -5,6 +5,8 @@ const nunjucks = require('nunjucks')
 const dataLoader = require('../lib/data-loader')
 const dateFormatter = require('../lib/date-formatter')
 const nameFormatter = require('../lib/name-formatter')
+const slugFormatter = require('../lib/slug-formatter')
+const goalSplitter = require('../lib/goal-splitter')
 
 const rootDir = path.join(__dirname, '..')
 const inputDir = 'src'
@@ -19,6 +21,9 @@ env.addFilter('firstName', nameFormatter.firstName)
 env.addFilter('surname', nameFormatter.surname)
 env.addFilter('initials', nameFormatter.initials)
 env.addFilter('dateFormatter', dateFormatter)
+env.addFilter('slugFormatter', slugFormatter)
+
+env.addGlobal('goalSplitter', goalSplitter)
 
 dataLoader.load().then(renderAll)
 
@@ -55,14 +60,12 @@ function renderMatchesOverview (data) {
 
 function renderMatches (data) {
   return Promise.all(data.matches.map(match => {
-    return renderViewToFile('match', match, formatMatchSlug(match))
+    return renderViewToFile('match', match, matchSlugFormatter(match))
   }))
 }
 
-function formatMatchSlug (match) {
-  const opponentSlug = match.opponent.replace(' ', '-').toLowerCase()
-  match.date = dateFormatter(match.date)
-  return `${matchesSlug}${match.date}/${opponentSlug}/`
+function matchSlugFormatter (match) {
+  return path.join('matches', dateFormatter(match.date), slugFormatter(match.opponent))
 }
 
 function renderViewToFile(view, data, slug) {
